b908fc7a514cb4c0aca0bc3adeb8e11aafbf4149
Presunto Parma <arthurgobbetti@gmail.com>
2026-05-10 08:51:59 -0300

    Added obstacle

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,14 +4,37 @@ public class PlayerController : MonoBehaviour
 {
     public float speed;
 
-    void Start()
+    public Transform target;
+    public float lerpSpeed = 1f;
+
+    public string tagToCheckEnemy = "Enemy";
+
+    private bool canRun;
+
+    private Vector3 pos;
+
+    private void Start()
     {
-        
+        canRun = true;
     }
 
-    // Update is called once per frame
     void Update()
     {
+        if (!canRun) return;
+
+        pos = target.position;
+        pos.y = transform.position.y;
+        pos.z = transform.position.z;
+
+        transform.position = Vector3.Lerp(transform.position, pos, speed * Time.deltaTime);
         transform.Translate(transform.forward * Time.deltaTime * speed);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag(tagToCheckEnemy))
+        {
+            canRun = false;
+        }
     }
 }
